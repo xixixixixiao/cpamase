698d1d05ff90ac02ac48d45717bc473ba78898ab
Xiaofeng Tan <xiaofeng_tan@live.com>
2020-09-27 16:40:37 +0800

    docs: 2.2 节 2

--- Ch02/Exercises/e01.c
+++ Ch02/Exercises/e01.c
@@ -1,5 +1,5 @@
 /**
- * 建立并运行由 Kernighan 和 Ritchie 编写的著名的“hello, world”程序：
+ * 1. 建立并运行由 Kernighan 和 Ritchie 编写的著名的“hello, world”程序：
  */
 
 #include <stdio.h>
@@ -12,6 +12,7 @@
 /**
  * 在编译时是否有警告信息？如果有，需要如何进行修改呢？
  * 
+ * 答:
  * 在 gcc version 4.9.2 中没有警告信息.
  * 
  * 应该添加 return 0;
---
+++ Ch02/Exercises/e02.c
@@ -0,0 +1,25 @@
+/**
+ * 2. 思考下面的程序：
+ */
+
+#include<stdio.h>
+
+int main(void){
+    printf("Parkinson's Law:\nWork expands so as to ");
+    printf("fill the time\n");
+    printf("available for its completion.\n");
+    return 0;
+}
+
+/**
+ * (a) 请指出程序中的指令和语句。
+ * (b) 程序的输出是什么？
+ */
+
+/**
+ * 答:
+ * 这个程序包含一条指令:
+ * #include
+ * 包含四条语句:
+ * 三条 printf 与一条 return.
+ */
